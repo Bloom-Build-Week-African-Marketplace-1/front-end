b21ac9e7dc889e568a7f21fd2db44ce2ca07df1a
Amethyst Willia <zmithic@gmail.com>
2021-12-21 11:58:19 -0600

    Added in actions I THINK it will work, the '/items' for the get request can be changed if that is incorrect

--- african-marketplace/src/state/actions/index.js
+++ african-marketplace/src/state/actions/index.js
@@ -3,3 +3,34 @@
 // Actions should be focused to a single purpose. 
 // You can have multiple action creators per file if it makes sense to the purpose those action creators are serving. 
 // Declare action TYPES at the top of the file
+
+import axiosWithAuth from '../../utils/axiosWithAuth';
+
+export const ITEM_START = 'ITEM_START';
+export const ITEM_SUCCEES = 'ITEM_SUCCESS';
+export const ITEM_ERROR = 'ITEM_ERROR';
+export const ITEM_ADD = 'ITEM_ADD';
+
+export const fetchItems = () => {
+    return (dispatch) => {
+        axiosWithAuth()
+            .get('/items') // I'm not sure if /items is the right url, my bad if it isn't
+                .then(resp => {
+                    dispatch ({type: ITEM_SUCCEES, payload: (resp.data)});
+            })
+                .catch(err => {
+                    dispatch ({type: ITEM_FAILURE, payload: (err)});
+            })
+    };
+};
+
+export const addItem = (newItem) => {
+    return {
+        type: ITEM_ADD,
+        payload: newItem,
+    };
+};
+
+export const setError = (err) => {
+    return ({type: ITEM_ERROR, payload: err});
+};
