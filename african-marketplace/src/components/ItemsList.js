aed17eeddbf6b0e8ff7170ef85c4a717cb9fa716
Amethyst Willia <zmithic@gmail.com>
2021-12-23 15:48:22 -0600

    Turned axiosWithAuth into a function because redux made me; added redux to ItemList

--- african-marketplace/src/components/ItemsList.js
+++ african-marketplace/src/components/ItemsList.js
@@ -1,32 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import API_URL from '../constants';
 import axiosWithAuth from '../utils/axiosWithAuth';
-import axios from 'axios';
+
+// Redux imports
+import { connect } from 'react-redux';
+import { fetchItems } from '../state/actions/index';
 
 const ItemsList = props => {
-  const initialState = [
-    {
-      category: '',
-      description: '',
-      item_id: 0,
-      location: '',
-      name: '',
-      price: 0,
-      user_id: 0,
-    },
-  ];
+  const { items, isLoading, itemError, fetchItems } = props;
+  // const initialState = [
+  //   {
+  //     category: '',
+  //     description: '',
+  //     item_id: 0,
+  //     location: '',
+  //     name: '',
+  //     price: 0,
+  //     user_id: 0,
+  //   },
+  // ];
 
-  const [items, setItems] = useState(initialState);
+  // const [items, setItems] = useState(initialState);
 
+  // useEffect(() => {
+  //   axiosWithAuth
+  //     .get(`${API_URL}items`)
+  //     .then(res => {
+  //       setItems(res.data);
+  //       console.log(res.data);
+  //     })
+  //     .catch(err => console.error(err));
+  // }, []);
+
+  // This should fetch the items using redux and therefore reducing code
   useEffect(() => {
-    axiosWithAuth
-      .get(`${API_URL}items`)
-      .then(res => {
-        setItems(res.data);
-        console.log(res.data);
-      })
-      .catch(err => console.error(err));
+    fetchItems();
   }, []);
+
+  // This is a loading page is isLoading === true
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  // This just puts up an error message if something goes wrong
+  if (itemError) {
+    const error = itemError.toString();
+    // No clue why it's making me toString but oh well
+    return <h2>Uh oh! We got this error: {error}</h2>;
+  }
 
   return (
     <div className="list-container">
@@ -47,4 +67,10 @@ const ItemsList = props => {
   );
 };
 
-export default ItemsList;
+const mapStateToProps = state => ({
+  items: state.itemReducer.items,
+  isLoading: state.itemReducer.isLoading,
+  itemError: state.itemReducer.itemError,
+});
+
+export default connect(mapStateToProps, { fetchItems })(ItemsList);
--- african-marketplace/src/utils/axiosWithAuth.js
+++ african-marketplace/src/utils/axiosWithAuth.js
@@ -2,16 +2,18 @@ import axios from 'axios';
 import API_URL from '../constants.js';
 
 // A axios calls to protected data
-
-const axiosWithAuth = axios.create({
-  baseURL: API_URL,
-  headers: {
-    Authorization:
-      typeof localStorage.getItem('token') === 'string'
-        ? localStorage.getItem('token')
-        : '',
-  },
-});
+// I had to turn axiosWithAuth into a function in order ot make redux work, sorry!
+const axiosWithAuth = () => {
+  return axios.create({
+    baseURL: API_URL,
+    headers: {
+      Authorization:
+        typeof localStorage.getItem('token') === 'string'
+          ? localStorage.getItem('token')
+          : '',
+    },
+  });
+};
 
 // at '/owner'  load in the owner's items
 // useEffect(() => {
