4f30bcf0ed2ab5ae384b679d315252630b640594
Phil G <fillupgalbraith@gmail.com>
2021-12-23 14:31:19 -0800

    all axios working

--- african-marketplace/src/components/ItemsList.js
+++ african-marketplace/src/components/ItemsList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
+import OwnerEditItem from './OwnerEditItem';
 
 const ItemsList = props => {
   const initialState = [
@@ -16,29 +17,84 @@ const ItemsList = props => {
 
   const [items, setItems] = useState(initialState);
 
-  useEffect(() => {
+  const [itemsChanged, setItemsChanged] = useState(0);
+
+  const getItems = () => {
     axiosWithAuth
       .get(`items`)
       .then(res => setItems(res.data))
       .catch(err => console.error(err));
+  };
+
+  useEffect(() => {
+    getItems();
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      getItems();
+    }, 2000);
+  }, [itemsChanged]);
+
+  const removeItem = e => {
+    axiosWithAuth
+      .delete(`items/${e.target.name}`)
+      .then(res => setItemsChanged(itemsChanged + 0.1))
+      .catch(err => console.error(err));
+  };
   return (
-    <div>
-      <div className="main-list">
-        {items.map(item => (
-          <figure key={item.id}>
-            <div className="image-price">
-              <img src={item.thumbnailUrl} alt={item.title} />
-              <p>{item.price}</p>
-            </div>
-            <figcaption>
-              <h3>{item.title}</h3>
-            </figcaption>
-          </figure>
-        ))}
+    <>
+      <div>
+        <div className="main-list">
+          {items
+            .filter(
+              item =>
+                item.user_id.toString() === localStorage.getItem('user_id')
+            )
+            .map(item => (
+              <figure key={item.item_id}>
+                <div className="image-price">
+                  <img src={item.thumbnailUrl} alt={item.name} />
+                  <p>{item.price}</p>
+                </div>
+                <figcaption>
+                  <h3>{item.title}</h3>
+                </figcaption>
+                <button name={item.item_id} onClick={removeItem}>
+                  Remove Item
+                </button>
+                <OwnerEditItem item={item} />
+              </figure>
+            ))}
+        </div>
       </div>
-    </div>
+      <div>
+        <div className="main-list">
+          {items
+            .filter(
+              item =>
+                !(item.user_id.toString() === localStorage.getItem('user_id'))
+            )
+            .map(item => (
+              <figure key={item.item_id}>
+                <div className="image-price">
+                  <img src={item.thumbnailUrl} alt={item.name} />
+                  <p>{item.price}</p>
+                </div>
+                <figcaption>
+                  <h3>{item.title}</h3>
+                </figcaption>
+                {item.user_id.toString() ===
+                  localStorage.getItem('user_id') && (
+                  <button name={item.item_id} onClick={removeItem}>
+                    Remove Item
+                  </button>
+                )}
+              </figure>
+            ))}
+        </div>
+      </div>
+    </>
   );
 };
 
